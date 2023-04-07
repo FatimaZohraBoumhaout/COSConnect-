3e9184720809969d7120edd875332bfcca5a5c1f
FatimaZohraBoumhaout <zb1949@princeton.edu>
2023-04-07 12:24:41 -0400

    From New Branch

--- Frontend/cosconnect/src/App.js
+++ Frontend/cosconnect/src/App.js
@@ -1,24 +1,35 @@
 import React from "react";
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation} from "react-router-dom";
 import './App.css';
 import UserProfileView from './Components/UserProfileView';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import UserSurvey from './Components/UserSurvey';
 
-function App() {
+function Layout({ page }) {
+  const location = useLocation();
+  const showHeaderFooter = location.pathname.startsWith('/profileview');
+
   return (
     <>
-    <Header />
-    <BrowserRouter>
-    <Routes>
-        <Route path="/" element={<UserSurvey />} />
-        <Route path="/profileview/:userId" element={<UserProfileView />} />
-    </Routes>
-    </BrowserRouter>
-    <Footer /> 
-   </>
+      {showHeaderFooter && <Header />}
+      {page}
+      {showHeaderFooter && <Footer />}
+    </>
   );
 }
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<UserSurvey />} />
+        <Route path="/profileview/:userId" element={<UserProfileView />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 export default App;
+
+
+
