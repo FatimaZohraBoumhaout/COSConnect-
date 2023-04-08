be0555d6597c24610b19a6caae559fb94c5e94de
Pascal Nabare <pascalnabare@princeton.edu>
2023-04-07 21:01:49 -0400

    app.js fix

--- Frontend/cosconnect/src/App.js
+++ Frontend/cosconnect/src/App.js
@@ -6,17 +6,27 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import UserSurvey from './Components/UserSurvey';
 
+function Layout({ page }) {
+  const location = useLocation();
+  const showHeaderFooter = location.pathname.startsWith('/profileview');
+
+  return (
+    <>
+      {showHeaderFooter && <Header />}
+      {page}
+      {showHeaderFooter && <Footer />}
+    </>
+  );
+}
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<UserSurvey />} />
         <Route path="/profileview/:userId" element={<UserProfileView />} />
-    </Routes>
+      </Routes>
     </BrowserRouter>
   );
 }
 export default App;
-
-
-
