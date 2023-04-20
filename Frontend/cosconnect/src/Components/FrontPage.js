1becd4cae05d388bf9165b643216455b8282af2a
FatimaZohraBoumhaout <zb1949@princeton.edu>
2023-04-20 03:04:45 -0400

    Front page

---
+++ Frontend/cosconnect/src/Components/FrontPage.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import "./FrontPage.css"; 
+
+function FrontPage() {
+
+    return(
+        <div>
+            <h1>Welcome to COSConnect where the journey begins!</h1>
+            <button>Log in to COSConnect</button>
+            <img src="FronPage.jpeg" alt="home page"/>
+        </div>
+    );
+}
+
+export default FrontPage;
