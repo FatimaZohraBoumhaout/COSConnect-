650109a3a6ca1862c974870deac24d230f7d628f
cor100 <cynthiacy.kjy@gmail.com>
2023-04-23 19:55:25 -0400

    debugging

--- Frontend/cosconnect/src/Components/ClassView.js
+++ Frontend/cosconnect/src/Components/ClassView.js
@@ -166,7 +166,7 @@ function ClassView(){
                     <center>
                     <form id="form" className="form">
                         <input placeholder="Search..." onChange={event => handleChange(event.target.value)}/>
-                        <button className="search-button">
+                        <button className="search-button" disabled={true}>
                         <svg viewBox="0 0 1024 1024"><path class="path1" d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z"></path></svg>
                         </button>
                     </form>  
@@ -186,7 +186,7 @@ function ClassView(){
                             <div style={{float:'right'}}>Sent</div>
                         </div>
                     ))}
-                    {sentRequest.length === 0 && 
+                    {sentRequest.length === 0 && receivedRequest.length === 0 &&
                         <center>
                             <p>Send some requests to get started.</p>
                         </center>
