beed6a0baf912ea8d568b0f92fdc37b07f270668
Ankhitha Manjunatha <am5897@princeton.edu>
2023-04-23 16:03:19 -0400

    class links works

--- Frontend/cosconnect/src/Components/ClassView.js
+++ Frontend/cosconnect/src/Components/ClassView.js
@@ -5,18 +5,13 @@ import { Link } from 'react-router-dom';
 import "./ClassView.css"
 import "@fontsource/inter";
 
-function ClassView(props){
+function ClassView(){
     const [cookies, setCookie] = useCookies(['net_id', 'partner_id', 'class_id']);
     const [classes, setClasses] = useState([]);
     const [sentRequest, setSentRequest] = useState([]);
     const [receivedRequest, setReceivedRequest] = useState([]);
     const [studentsId, setStudentsId] = useState([]);
     const [input, setInput] = useState('');
-    const [disabled, setDisabled] = useState({receiver_id: 'false'})
-    const net_id = cookies.net_id;
-    const location = useLocation;
-    const buttonRef= useRef(null);
-    const [studentInfo, setStudentInfo] = useState([]);
     
     const this_class = window.location.search.match(/class=([^&]*)/)[1].replace('%20',' ');
 
@@ -63,19 +58,6 @@ function ClassView(props){
             console.log("net_id is null");
           }
       }, [cookies.net_id]);
-    
-      /* useEffect(() => {
-        if (cookies.net_id !== null) {
-            fetch(`/get_students?id=${cookies.net_id}&class=${this_class}`)
-            .then(response=> response.json())
-            .then(data => {
-                setStudentsId(data);
-            })
-            .catch(error => console.log(error));
-          } else {
-            console.log("net_id is null");
-          }
-      }, [cookies.net_id]); */
 
       useEffect(() => {
         if (cookies.net_id !== null) {
@@ -89,54 +71,21 @@ function ClassView(props){
             console.log("net_id is null");
           }
       }, [cookies.net_id]);
-    //   ).then((body) =>{
-    //     body.forEach(response =>{
-    //         if(response){
-    //             response.json().then(data=>{
-    //                 studentInfo.push(data)
-    //             })
-    //         }else{
-    //             console.log(`Response error: ${response.status}`);
-    //         }
-    //     })
-    // var studentInfo = []
-    // Promise.all(
-    //     studentsId.map(id => {
-    //         fetch(`/get_info?id=${id}`)
-    //         .then(response => response.json())
-    //         .then(json=>{
-    //             studentInfo.push(json)
-    //         })
-    //     })
-    // )
-    function handleChange(event){
-        //setData()
-        console.log(event)
-    }
-    function handleSubmit(event){
-        //event.preventDefault();
-        //console.log(data);
-    }
+
     let renderClasses = null;
     if(classes.length > 0){
         renderClasses = classes[0][0] && classes[0][0].map((cl) => {
             return(
                 <div className="rectangle-left">
-                    <center>
-                        <Link to={`/classview?class=${cl}`}>
-                        {cl}
-                        </Link>
-                    </center>
+                    <Link to={`/classview?class=${cl}`} onClick={() => window.location.replace(`/classview?class=${cl}`)}><center>{cl}</center></Link>
                 </div>
             );
         });
     }
+    
     function sendRequest(st) {
         setCookie('class_id', this_class)
-        // fetch(`add_request?sender_id=${cookies.net_id}&receiver_id=${st}&course=${this_class}`, {
-        //     method: 'POST',
-        // })
-        //if(buttonRef.current) buttonRef.current.disabled = true;
+        setCookie('partner_id', st);
     }
     function handleClick(st) {
         setCookie('partner_id', st);
@@ -151,7 +100,7 @@ function ClassView(props){
                     <center>NetId: {st[0]} <p>Display Name: {st[1]}, Availability: {st[2]}</p></center>
                 </div>
                 </Link>
-                <Link className="btn" onClick={sendRequest(st[0])} to={`/sendrequest`}>
+                <Link className="btn" onClick={() => sendRequest(st[0])} to={`/sendrequest`}>
                     <center>Send</center>
                 </Link>
             </div>
@@ -170,7 +119,7 @@ function ClassView(props){
                 <div className="search">
                     <center>
                     <form id="form" className="form">
-                        <input placeholder="Search..." onChange={event => handleChange(event.target.value)}/>
+                        <input placeholder="Search..." value={input}></input>
                         <button className="search-button">
                         <svg viewBox="0 0 1024 1024"><path class="path1" d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z"></path></svg>
                         </button>
