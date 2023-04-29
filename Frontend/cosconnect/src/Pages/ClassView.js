37c8415ef3986ef4cab07beaa7eaeaf2b1017d6f
cor100 <cynthiacy.kjy@gmail.com>
2023-04-29 15:20:23 -0400

    search by availability

--- Frontend/cosconnect/src/Pages/ClassView.js
+++ Frontend/cosconnect/src/Pages/ClassView.js
@@ -16,6 +16,7 @@ function ClassView(){
     const [receivedRequest, setReceivedRequest] = useState([]);
     const [studentsId, setStudentsId] = useState([]);
     const [input, setInput] = useState('');
+    const [availInput, setAvailInput] = useState('');
     //const [finaloutput, setFinalOutput] = useState([]);
     const [renderStudents, setRenderStudents] = useState([]);
     const [fixed, setFixed] = useState([]);
@@ -105,8 +106,10 @@ function ClassView(){
     }
 
     useEffect(() => {
-         output = fixed.filter(element => element[1].includes(input))
          console.log("fixed from input set to be", fixed)
+
+         output = fixed.filter(element => (element[1].includes(input) && 
+                                            element[2].includes(availInput)))
          output = output.map(element => element[0])
          //setFinalOutput(output)
          let studentsToRender = studentsId.map((st, index) =>{
@@ -118,14 +121,14 @@ function ClassView(){
                 </div>
                 </Link>
                 <Link className="btn" onClick={() => sendRequest(st[0])} to={`/sendrequest`}>
-                    <center>Send</center>
-                </Link>
+                    <center >Send</center>
+                </Link >
             </div>);
             }});
         setRenderStudents(studentsToRender);
 
         console.log("output set to be", output)
-    }, [input, fixed]);
+    }, [input, availInput, fixed]);
     
     function sendRequest(st) {
         setCookie('class_id', this_class)
@@ -144,10 +147,14 @@ function ClassView(){
         setInput(event)
     }
 
+    function handleAvailChange(event){
+        setAvailInput(event);
+    }
+
     useEffect(() => {
         if(studentsId && studentsId.length > 0){
             console.log("StudentsId set to", studentsId)
-            setFixed(studentsId.map((element, index) => [index, element[1]]))
+            setFixed(studentsId.map((element, index) => [index, element[1], element[2]]))
             console.log("fixed set to", fixed)
             output = fixed.map(element => element[0])
 
@@ -183,7 +190,12 @@ function ClassView(){
                 <div className="search">
                     <center>
                     <form id="form" className="form">
-                        <input placeholder="Search..." onChange={event => handleChange(event.target.value)}/>
+                        <input placeholder="Search Name..." onChange={event => handleChange(event.target.value)}/>
+                        <button className="search-button" disabled={true}>
+                        <svg viewBox="0 0 1024 1024"><path class="path1" d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z"></path></svg>
+                        </button>
+                        
+                        <input placeholder="Search Availability..." onChange={event => handleAvailChange(event.target.value)}/>
                         <button className="search-button" disabled={true}>
                         <svg viewBox="0 0 1024 1024"><path class="path1" d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z"></path></svg>
                         </button>
@@ -194,7 +206,7 @@ function ClassView(){
                     <h3>Invitations</h3>
                     {sentRequest && sentRequest.map((req) => (
                         <div className="rectangle-right">
-                            <center>{req}</center>
+                            <div><center>{req}</center></div>
                             <div style={{float:'right'}}>Sent</div>
                         </div>
                     ))}
