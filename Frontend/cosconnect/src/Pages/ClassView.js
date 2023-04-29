76d3d5fe7b3a7fc60b0e364a5313db0f1bec55a2
cor100 <cynthiacy.kjy@gmail.com>
2023-04-29 15:33:56 -0400

    search by name, netid, availability

--- Frontend/cosconnect/src/Pages/ClassView.js
+++ Frontend/cosconnect/src/Pages/ClassView.js
@@ -108,7 +108,7 @@ function ClassView(){
     useEffect(() => {
          console.log("fixed from input set to be", fixed)
 
-         output = fixed.filter(element => (element[1].includes(input) && 
+         output = fixed.filter(element => (element[1].includes(input) || element[3].includes(input) && 
                                             element[2].includes(availInput)))
          output = output.map(element => element[0])
          //setFinalOutput(output)
@@ -154,7 +154,7 @@ function ClassView(){
     useEffect(() => {
         if(studentsId && studentsId.length > 0){
             console.log("StudentsId set to", studentsId)
-            setFixed(studentsId.map((element, index) => [index, element[1], element[2]]))
+            setFixed(studentsId.map((element, index) => [index, element[1], element[2], element[0]]))
             console.log("fixed set to", fixed)
             output = fixed.map(element => element[0])
 
@@ -190,7 +190,7 @@ function ClassView(){
                 <div className="search">
                     <center>
                     <form id="form" className="form">
-                        <input placeholder="Search Name..." onChange={event => handleChange(event.target.value)}/>
+                        <input placeholder="Search Name or NetId..." onChange={event => handleChange(event.target.value)}/>
                         <button className="search-button" disabled={true}>
                         <svg viewBox="0 0 1024 1024"><path class="path1" d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z"></path></svg>
                         </button>
